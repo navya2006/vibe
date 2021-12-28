c3435418b9a0d2d6f2f816feb24ab4b0c9adcba6
Naahvya <46841818+navya2006@users.noreply.github.com>
2021-12-28 17:17:34 +0530

    Add files via upload

---
+++ e/index.js
@@ -0,0 +1,7 @@
+  // SIDENAV 
+  function openNav(){
+      document.getElementById("sidenav").style.width="100%";
+  }
+  function closeNav () {
+      document.getElementById("sidenav").style.width="0";
+  }
